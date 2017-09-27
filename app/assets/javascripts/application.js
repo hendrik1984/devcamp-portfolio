93be02c0f83c154f17d5a274ce9dda0d969af3f3
hendrik.helianto <hendrik.helianto@reebonz.com>
2017-09-27 13:22:58 +0700

    Implemented gritter for notifications

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
-//= require turbolinks
 //= require cocoon
+//= require gritter
+//= require turbolinks
 //= require_tree .
